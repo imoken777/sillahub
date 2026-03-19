758c5f1a797c67c1e894ae824c40208127e58d60
imoken777 <imoken27@gmail.com>
2026-03-19 22:39:44 +0900

    refactor: revalidateRootPageのAPI呼び出しを削除

--- src/workers/custom-worker.ts
+++ src/workers/custom-worker.ts
@@ -1,7 +1,5 @@
-import type { ApiType } from '@/app/api/[...route]/route';
 import { getDb } from '@/lib/db';
 import { updateSyllabusService } from '@/services/syllabus';
-import { hc } from 'hono/client';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore `.open-next/worker.ts` is generated at build time
@@ -9,13 +7,11 @@ import { default as handler } from '../../.open-next/worker';
 
 const scheduled: ExportedHandlerScheduledHandler<CloudflareEnv> = (_, env, ctx) => {
   const db = getDb(env.TURSO_DATABASE_URL, env.TURSO_AUTH_TOKEN);
-  const apiClient = hc<ApiType>(env.APP_ORIGIN_URL);
 
   ctx.waitUntil(
     updateSyllabusService(db).then((result) =>
       result.match(
-        async (res) => {
-          await apiClient.api.revalidateRootPage.$put().catch(console.error);
+        (res) => {
           // eslint-disable-next-line no-console
           console.log('Syllabus update completed:', res.count);
         },
